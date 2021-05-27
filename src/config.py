878cc7bbf88c221233a641267b51eba8ba32d875
Jacob Nielsen <jnillern@gmail.com>
2021-05-27 18:10:36 +0200

    Readd config.py

---
+++ src/config.py
@@ -0,0 +1,14 @@
+config = {
+    "binance_keys": {
+        "binanceaccount1": {"API_KEY": os.environ.get('BINANCE_API_KEY', ""), "SECRET_KEY": os.environ.get('BINANCE_SECRET_KEY', "")},
+    },
+    "bitmex_keys": {
+        "bitmexaccount1": {"API_KEY": os.environ.get('BITMEX_API_KEY', ""), "SECRET_KEY": os.environ.get('BITMEX_SECRET_KEY', "")},
+        "bitmexaccount2": {"API_KEY": "", "SECRET_KEY": ""}
+    },
+    "bitmex_test_keys": {
+        "bitmextest1": {"API_KEY": "", "SECRET_KEY": ""},
+        "bitmextest2": {"API_KEY": "", "SECRET_KEY": ""}
+    },
+    "line_apikey": {"API_KEY": ""}
+}
