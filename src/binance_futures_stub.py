2868389a373521cb3f76c206e49190a33e81fb71
Jacob Nielsen <jnillern@gmail.com>
2021-05-28 10:29:39 +0200

    Change 'is' to '=='

--- src/binance_futures_stub.py
+++ src/binance_futures_stub.py
@@ -142,7 +142,7 @@ class BinanceFuturesStub(BinanceFutures):
         : param post_only: post only
         : param when: Do you order?
         : return:
-         """
+        """
         if not when:
             return
 
@@ -260,7 +260,7 @@ class BinanceFuturesStub(BinanceFutures):
             if self.balance_ath < self.balance:
                 self.balance_ath = self.balance
             if self.balance_ath > self.balance:
-                if self.max_draw_down_session is 0:
+                if self.max_draw_down_session == 0:
                     self.max_draw_down_session = self.balance_ath - self.balance
                     self.max_draw_down_session_perc = (
                         self.balance_ath - self.balance) / self.balance_ath * 100
