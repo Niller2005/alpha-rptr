60a2a4a7649c8ec16f0cd663fa556ff3b791db9a
Jacob Nielsen <jnillern@gmail.com>
2021-05-30 12:16:10 +0200

    YYY: change limit to percent and cross+trend back to entry

--- src/strategy.py
+++ src/strategy.py
@@ -516,15 +516,15 @@ class YYY(Bot):
 
         if not eval(os.environ.get('BOT_TEST', 'False')):
             if dead_cross and uptrend:
-                self.exchange.order("Long", True, lot, limit=price-0.5, when=True, post_only=True)
+                self.exchange.entry("Long", True, lot, limit=price/1.001, when=True, post_only=True)
                 logger.info('in dead_cross and uptrend for long')
 
             if float(self.exchange.get_position()['notional']) > 0.0:
-                self.exchange.order("Long", False, lot, limit=price+0.5, stop=(price+0.5), when=golden_cross, post_only=True)
+                self.exchange.order("Long", False, lot, limit=price*1.001, stop=(price*1.001), when=golden_cross, post_only=True)
 
             if golden_cross and downtrend:
-                self.exchange.order("Short", False, lot, limit=price+0.5, when=True, post_only=True)
+                self.exchange.entry("Short", False, lot, limit=price*1.001, when=True, post_only=True)
                 logger.info('in golden_cross and downtrend for short')
 
             if float(self.exchange.get_position()['notional']) < 0.0:
-                self.exchange.order("Short", True, lot, limit=price-0.5, stop=(price-0.5), when=dead_cross, post_only=True)
+                self.exchange.order("Short", True, lot, limit=price/1.001, stop=(price/1.001), when=dead_cross, post_only=True)
