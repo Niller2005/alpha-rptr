be07b12d9a0c29a6d694102904d56b0fb69758fa
Jacob Nielsen <jnillern@gmail.com>
2021-05-31 17:37:03 +0200

    YYY: Logging formatting

--- src/strategy.py
+++ src/strategy.py
@@ -506,7 +506,7 @@ class YYY(Bot):
 
         logger.info(f'--------------------------------------')
         logger.info(f'trend: {ct}')
-        logger.info(f'next cross: {nc if (nc == "golden" and np == "short") or (nc == "dead" and np == "long") else nc + "(Skipping)" } > {np} > {lot} @ {calc_entry_price(price, False, self.price_decimal_num) if np == "short" else calc_entry_price(price, True, self.price_decimal_num)}')
+        logger.info(f'next cross: {nc if (nc == "golden" and np == "short") or (nc == "dead" and np == "long") else nc + " (Skipping)" } > {np} > {lot} @ {calc_entry_price(price, False, self.price_decimal_num) if np == "short" else calc_entry_price(price, True, self.price_decimal_num)}')
         if (trend_sma[-1] != trend_sma[-1] or trend_sma[-3] != trend_sma[-3] or trend_sma[-10] != trend_sma[-10]):
             logger.info(f'--------------------------------------')
             logger.info(f'Bot status: NEEDS RESTART')
